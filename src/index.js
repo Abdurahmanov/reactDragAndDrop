5d419375cfac7f2274bbef5c1840cfa6ec383e5d
artur <artur@winhappens.com>
2018-05-29 00:12:48 +0300

    add style and components in the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/global.scss';
+import './style/global.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
