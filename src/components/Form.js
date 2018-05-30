088aec0ae0652155ebba6c44acb5f91b43486d14
artur <artur@winhappens.com>
2018-05-31 00:57:16 +0300

    add error

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,49 +2,117 @@ import React, { Component } from 'react';
 
 class Form extends Component {
     render() {
+        let i = 1
+        let fileList = this.state.fileArr.map( (name) => {
+            return <div key={i++}>
+                <div>Имя файла - {name.name}</div>
+                <div>Кол-во объектов в json - {name.length}</div>
+            </div>;
+        });
         return (
-            <form className="form">
-                <input className="form__input" type="file" name="file" id="file" onChange={this.onFileDrop.bind(this)}/>
-                <label htmlFor="file" className="form__label">Загрузить json</label>
-                <div>{this.state.file.name}</div>
-                <div>Размер {this.state.file.size}</div>
-                <div>Кол-во объектов {this.state.length}</div>
-            </form>
+            <div className="form__block">
+                <form className="form" id="form">
+                    <input className="form__input" type="file" name="file" id="file" onChange={this.onFileDrop.bind(this)} onDragOver={this.dragOver} onDragLeave={this.dragLeave}/>
+                    <label htmlFor="file" className="form__label">Загрузить json</label>
+
+                </form>
+                <div className="form__List">
+                   {fileList}
+                </div>
+            </div>
+
+
 
         );
     }
+
+
     constructor(props) {
         super(props);
 
         this.state = {
-            file: '',
-            length:''
+            errorMsg:'',
+            fileArr:[]
         };
     }
     componentDidMount(){
 
     }
 
+
+    dragStart(){
+        console.log('pek')
+        // let el = document.getElementById('file')
+        // el.classList.add('form_hover')
+    }
+
+    dragOver(){
+        let el = document.getElementById('form')
+        el.classList.add('form_hover')
+    }
+
+    dragLeave(){
+        let el = document.getElementById('form')
+        el.classList.remove('form_hover')
+    }
+
     onFileDrop(e){
         console.log(e.target.files[0])
-        let f = e.target.files[0]
-        let reader = new FileReader();
-        reader.onloadend = e =>{
-            let arr = JSON.parse(e.target.result)
-            if(!Array.isArray(arr)){
-                this.setState({
-                    length:[arr].length
-                });
-            } else{
-                this.setState({
-                    length:arr.length
-                });
-            }
+        let el = document.getElementById('form')
+        let fileList = this.state.fileArr
+        if(el.classList.contains('form_hover')){
+            el.classList.remove('form_hover')
         }
-        reader.readAsText(f);
-        this.setState({
-            file:f
-        });
+
+        let f = e.target.files[0]
+        console.log(f.type)
+        let reader = new FileReader();
+        if(f.type !== 'application/json'){
+            console.log(`Вы загрузили ${f.type}, так не пойдет! Загрузите файл json`)
+        } else if(f.size === 0){
+            console.log(`Вы загрузили пустой json`)
+        } else{
+            reader.onloadend = e =>{
+                try {
+                    let arr = JSON.parse(e.target.result)
+                    if(!Array.isArray(arr)){
+                        fileList.push({
+                            length:[arr].length,
+                            name:f.name
+                        })
+                        this.setState({
+                            fileArr:fileList
+                        });
+                    } else{
+                        fileList.push({
+                            length:arr.length,
+                            name:f.name
+                        })
+                        this.setState({
+                            fileArr:fileList
+                        });
+                    }
+                } catch (error){
+                    console.log(error)
+                    if(error.message.indexOf("token '") !== -1){
+                        console.log('Ошибка! Пристутствуют недопустимые одинарные кавычки')
+                    } else if(error.message.indexOf("token ,") !== -1){
+                        console.log('Ошибка! Отсутсвуют ключ/значение')
+                    } else if(error.message.indexOf("token ") !== -1){
+                        console.log('Ошибка! Отсутсвуют "" кавычки')
+                    } else{
+                        this.setState({
+                            errorMsg:error
+                        });
+                    }
+
+
+                }
+            }
+            reader.readAsText(f);
+        }
+
+
 
 
     }
